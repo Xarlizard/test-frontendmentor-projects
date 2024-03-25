05371b62cd2c64b325c3943dea387174c94d0cdf
xarlizard <charkrios@gmail.com>
2024-03-25 12:10:11 +0100

    Completed ✅

--- 17-advice-generator-app/script.js
+++ 17-advice-generator-app/script.js
@@ -1,38 +1,33 @@
 
-const apiUrl = 'https://api.adviceslip.com/advice';
-let number = document.getElementById("number");
-let advice = document.getElementById("advice");
+const apiUrl = 'https://api.adviceslip.com/advice/';
+const number = document.getElementById("number");
+const advice = document.getElementById("advice");
+let degrees = 0;
+newFetch();
 
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    number.textContent = data.slip.id;
-    advice.textContent = data.slip.advice;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+function newFetch(){
+  let randomNum = Math.floor(Math.random() * 225);
+  let randomUrl = apiUrl + `${randomNum}`;
+  fetch(randomUrl)
 
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      number.textContent = data.slip.id;
+      advice.textContent = data.slip.advice;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
 
-  function newFetch(){
-    console.log("Button pressed");
-fetch(apiUrl)
-.then(response => {
-    if (!response.ok) {
-    throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    number.textContent = data.slip.id;
-    advice.textContent = data.slip.advice;
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+function rotateImage() {
+  degrees += 270;
+  var myImage = document.getElementById("circle");
+  myImage.style.transform = `rotate(${degrees}deg)`;
+
 }
